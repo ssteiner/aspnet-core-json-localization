0363cf91c2902a7c83dc67e2d28b1af21ba580e0
Stephan Steiner <stephan.steiner@gmail.com>
2023-01-06 17:42:04 +0100

    added documentation for JsonTranslationOptions

--- JsonLocalizationLib/JsonTranslationOptions.cs
+++ JsonLocalizationLib/JsonTranslationOptions.cs
@@ -2,10 +2,19 @@
 {
     public class JsonTranslationOptions
     {
+        /// <summary>
+        /// full path to the resource files
+        /// </summary>
         public string ResourcePath { get; set; }
 
+        /// <summary>
+        /// path of the resources, relative to the application (empty = Resources)
+        /// </summary>
         public string ResourceFolder { get; set; }
 
+        /// <summary>
+        /// two character ISO code of the fallback culture
+        /// </summary>
         public string FallbackCulture { get; set; }
     }
 }
